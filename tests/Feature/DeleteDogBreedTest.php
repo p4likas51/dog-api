5ea40543d3df6bf7e0de291f64339138aaf312c2
fazekasbotond3 <fazekas.botond@students.jedlik.eu>
2026-01-15 11:49:12 +0100

    test#6

--- tests/Feature/DeleteDogBreedTest.php
+++ tests/Feature/DeleteDogBreedTest.php
@@ -10,4 +10,8 @@ it('deletes a dog breed', function () {
     expect(DogBreed::where('id', $breed->id)->exists())->toBeFalse();
 });
 
+it('returns 404 when deleting missing breed', function () {
+    $this->deleteJson('/api/dog-breeds/99999')->assertNotFound();
+});
+
 
